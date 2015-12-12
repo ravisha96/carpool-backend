04ec154ad7bac6441743a4cd7d3932bfda8924ef
ravi kumar sha <ravi.sha@nagarro.com>
2015-12-12 15:12:58 +0530

    registering a dummy driver

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,19 +1,43 @@
-define(["require", "exports", 'express'], function (require, exports, express) {
+define(["require", "exports", 'express', 'mongoose'], function (require, exports, express, db) {
     var viewRenderingEngine = require('ejs-mate');
+    var bodyParser = require('body-parser');
     var app = express();
+    db.connect('mongodb://nagarro:carpool@ds035014.mongolab.com:35014/heroku_55xgjmp3');
+    var CordinateSchema = new db.Schema({
+        name: String,
+        lng: Number,
+        lat: Number
+    });
+    var AdminSchema = new db.Schema({
+        uid: Number,
+        name: String,
+        from: CordinateSchema,
+        to: CordinateSchema,
+        boardingPoints: [CordinateSchema],
+        startTime: Date,
+        returnTime: Date,
+        price: Number,
+        seats: Number,
+        catType: String,
+        remarks: String,
+        routeId: String,
+        createdOn: { type: Date, default: Date.now }
+    });
+    db.model('Admin', AdminSchema);
     app.engine('html', viewRenderingEngine);
-    app.set('view engine', 'html'); // so you can render('index') 
+    app.set('view engine', 'html'); // so you can render('index')
+    app.use(bodyParser.json());
     app.get('/', function (req, res) {
         res.render('index');
     });
-    app.get('/api/coolresource', function (req, res) {
+    app.get('/api/register', function (req, res) {
         res.send({
             "isApiWorking": false,
             "name": "Maximilian Alexander"
         });
     });
     var port = process.env.PORT || 3000;
-    var server = app.listen(8000, function () {
+    var server = app.listen(port, function () {
         var listeningPort = server.address().port;
         console.log('The server is listening on port: ' + listeningPort);
     });
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,21 +1,85 @@
 /// <reference path="../../typings/tsd.d.ts" />
 import express = require('express');
 import path = require('path');
+import db = require('mongoose');
 var viewRenderingEngine = require('ejs-mate');
-
+var bodyParser = require('body-parser');
 var app : express.Express = express();
 
+
+db.connect('mongodb://nagarro:carpool@ds035014.mongolab.com:35014/heroku_55xgjmp3');
+
+var CordinateSchema = new db.Schema({
+	name: String,
+	lng: Number,
+	lat: Number
+});
+
+var DriverSchema = new db.Schema({
+	uid: Number,
+	name: String,
+	from: CordinateSchema,
+	to: CordinateSchema,
+	boardingPoints: [CordinateSchema],
+	startTime: Date,
+	returnTime: Date,
+	price: Number,
+	seats: Number,
+	catType: String,
+	remarks: String,
+	routeId: String,
+	createdOn: {type: Date, default: Date.now}
+});
+
+var Driver = db.model('Driver', DriverSchema);
+
 app.engine('html', viewRenderingEngine);
-app.set('view engine', 'html'); // so you can render('index') 
+app.set('view engine', 'html'); // so you can render('index')
+
+/** MIDDLEWARES */
+
+// parse all request and response in json.
+app.use(bodyParser.json());
+
+// Enable CORS
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    next();
+});
 
 app.get('/', (req, res) => {
-	res.render('index');  
-}); 
+	res.render('index');
+});
 
-app.get('/api/coolresource', (req, res) => {
-	res.send({
-		"isApiWorking": false,
-		"name": "Maximilian Alexander"
+app.get('/api/register', (req, res) => {
+	var newDriver = new Driver({
+		uid: 1001,
+		name: 'Ravi Kumar Sha',
+		from: {
+			name: 'Gurgaon, Sector-57',
+			lng: 28.4880432,
+			lat: 77.0622027
+		},
+		to: {
+			name: 'Nagarro, Sector-18',		
+			lng: 28.490027,
+			lat: 77.0255228
+		},
+		boardingPoints: [],
+		startTime: new Date(),
+		price: 20,
+		seats: 3,
+		catType: 'Hatchback',
+		remarks: 'Hello World',
+		routeId: 'PG4#$L1',
+		createdOn: {type: Date, default: Date.now}
+	});
+	
+	newDriver.save(function (err) {
+		res.status(200).json(newDriver);
 	});
 });
 
