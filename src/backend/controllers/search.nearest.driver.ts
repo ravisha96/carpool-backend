bc071a7fd7461c8884a919bcc5c162b29456b8e2
ravi kumar sha <ravi.sha@nagarro.com>
2016-01-06 00:11:18 +0530

    server-side promise implemented

--- src/backend/controllers/search.nearest.driver.ts
+++ src/backend/controllers/search.nearest.driver.ts
@@ -8,41 +8,44 @@
 import Utilities = require('./utility');
 
 interface ISearchNearestDriver {
-    
+
 }
 
 class SearchNearestDriver extends Utilities {
-    
+
     private driversCurrentLocSchema = require('../models/update.current.location.schema');
-    private _und = require ('underscore');  // underscore library
+    private Promise = require('q');
+    private und = require('underscore');  // underscore library
     
     constructor(public coords) {
         super();
-        this.fetchDriversLocation();
     }
     
     /**
      * fetchDriversLocation method make a DB call to retrive all the drivers list.
      */
-    private fetchDriversLocation = () => {
+    public fetchDriversLocation = () => {
         var distance = [],
-            _ = this._und;
-        this.driversCurrentLocSchema.find( (err, results) => {
-            _.forEach(results, (result) => {
-                /**
-                 * Setting both date on same format.
-                 */
-                var passengerTravelingDate = new Date(this.coords.time),
-                    driverTravelingDate = new Date(result.departureTime);
-                    
-                if(!this.matchDriverTimingWithPassenger(passengerTravelingDate.toISOString(), driverTravelingDate.toISOString())) return;
-                result._doc.distance = this.getDistanceBtwnLatLng(this.coords.boarding, result);
-                distance.push(result._doc);
+            defer = this.Promise.defer(),
+            _ = this.und;
+
+            this.driversCurrentLocSchema.find((err, results) => {
+                _.forEach(results, (result) => {
+                    /**
+                     * Setting both date on same format.
+                     */
+                    var passengerTravelingDate = new Date(this.coords.time),
+                        driverTravelingDate = new Date(result.departureTime);
+
+                    if (!this.matchDriverTimingWithPassenger(passengerTravelingDate.toISOString(), driverTravelingDate.toISOString())) return;
+                    result._doc.distance = this.getDistanceBtwnLatLng(this.coords.boarding, result);
+                    distance.push(result._doc);
+                });
+                
+                defer.resolve(distance);
             });
             
-            return distance;
-            
-        });
+            return defer.promise;
     }
     
     /**
@@ -52,7 +55,7 @@ class SearchNearestDriver extends Utilities {
     private matchDriverTimingWithPassenger = (passTiming: String, driverTiming: String) => {
         return this.startDate(passTiming).endDate(driverTiming).isGreaterThanEqual();
     }
-    
+
 }
 
 module.exports = SearchNearestDriver;
--- src/backend/router.ts
+++ src/backend/router.ts
@@ -1,70 +1,72 @@
 import mongoose = require('mongoose');
-var DriverModel = require('./models/driver.register.schema');
-var LoginModel = require('./models/user.login.schema');
-var PassengerModel = require('./models/passenger.register.schema');
-var UpdateCurrentLocationModel = require('./models/update.current.location.schema');
-
-
-var SearchNearestDriversCtrl = require('./controllers/search.nearest.driver'); 
 
 class Router {
-	
-	constructor (public db:any) {
-		this.db = db;
-	}
-    
+
+    private DriverModel = require('./models/driver.register.schema');
+    private LoginModel = require('./models/user.login.schema');
+    private PassengerModel = require('./models/passenger.register.schema');
+    private UpdateCurrentLocationModel = require('./models/update.current.location.schema');
+    private SearchNearestDriversCtrl = require('./controllers/search.nearest.driver');
+    private Promise = require('q');
+
+
+    constructor(public db: any) {
+        this.db = db;
+    }
+
     public Authenticate = (req, res) => {
-		var loginSchema = new LoginModel(req.body);
-		loginSchema.save( (): void => {
-			res.send(loginSchema);
-		});
-	}
-    
+        var loginSchema = new this.LoginModel(req.body);
+        loginSchema.save((): void => {
+            res.send(loginSchema);
+        });
+    }
+
     public LoginDriver = (req, res) => {
-		LoginModel.findOne(req.body, (err: String, results: Object) => {
-			res.send(results);
-		});
-	}
-	
-	public RegisterDriver = (req, res) => {
-        
+        this.LoginModel.findOne(req.body, (err: String, results: Object) => {
+            res.send(results);
+        });
+    }
+
+    public RegisterDriver = (req, res) => {
+
         var driver: IDriverRegistration = req.body;
-		var driverSchema = new DriverModel(driver);
-        
-        var locationSchema = new UpdateCurrentLocationModel({
+        var driverSchema = new this.DriverModel(driver);
+
+        var locationSchema = new this.UpdateCurrentLocationModel({
             uid: driver.uid,
             lat: driver.from.lat,
             lng: driver.from.lng,
             departureTime: driver.startTime
         });
-        
-		driverSchema.save( (err, results): void => {
+
+        driverSchema.save((err, results): void => {
             console.log(results);
-            locationSchema.save( (): void => {
-               res.send(results);
+            locationSchema.save((): void => {
+                res.send(results);
             });
-		});
-	}
+        });
+    }
     
     /**
      * Method will register the searched query in DB and will send a response
      * to the nearest/closest driver.
      */
     public SearchNearestDrivers = (req, res) => {
-        var passengerSchema = new PassengerModel(req.body);
+        var passengerSchema = new this.PassengerModel(req.body);
         
-        passengerSchema.save( (): void => {
-           res.send(new SearchNearestDriversCtrl(req.body));
-        });
+        new this.SearchNearestDriversCtrl(req.body).fetchDriversLocation()
+            .then((data) => {
+                res.send(data);
+            });
     }
-    
+
     public UpdateCurrentLocation = (req, res) => {
-        var condition = {uid: req.body.uid}, update = {lat: req.body.lat, lng: req.body.lng};
-        UpdateCurrentLocationModel.update(condition, update, (err, numberAffected, response) => {
+        var condition = { uid: req.body.uid }, update = { lat: req.body.lat, lng: req.body.lng };
+        this.UpdateCurrentLocationModel.update(condition, update, (err, numberAffected, response) => {
             res.send(req.body);
         });
     }
-	
+
 }
 
 export = Router;
